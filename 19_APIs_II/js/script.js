9bdeef63525e757fb0cada26c3a97be0d6bf343e
xk08 <gronytzki@gmail.com>
2021-11-22 15:55:06 -0300

    Aula 19

---
+++ 19_APIs_II/js/script.js
@@ -0,0 +1,48 @@
+//Capturando as interações feitas nos elementos HTML
+let title = document.querySelector('#title');
+let desc = document.querySelector('#desc');
+let btn = document.querySelector('#btn');
+
+//Chamando a função de cadastro
+btn.addEventListener('click', cadastrarNovoPost);
+
+//Está sendo utilizado Async/Await (Função Assincrona)
+async function cadastrarNovoPost(event) {
+
+  event.preventDefault();
+
+  //Obtendo url da API
+  let url = 'https://jsonplaceholder.typicode.com/posts';
+
+  //Montando objeto js que será enviado a API
+  let post = {
+    title: title.value, //captura o valor presente no campo
+    desc: desc.value
+  }
+
+  //convetendo objeto js para Json
+  let postString = JSON.stringify(post);
+
+  //Criando objeto 'params' que será enviado na requisição
+  let params = {
+    method: 'POST', //endpoint
+    headers: { //cabeçalho
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+    body: postString //corpo da requisição
+  }
+
+  // Chamando a requisição no endpoint da API
+  let response = await fetch(url, params); //Precisa ser assincrono
+
+  //Obtendo o "data" do response que foi pego na requisição
+  var postJson = await response.json(); //Precisa ser assincrono
+
+  console.log(postJson);
+
+  //Exibindo o id retornado pela API na tag <p> da pagina HTML
+  document.querySelector('p').innerText = `Post cadastrado com sucesso! Id = ${postJson.id}`;
+
+}
+
+
