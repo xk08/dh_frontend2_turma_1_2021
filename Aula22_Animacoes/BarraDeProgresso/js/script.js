727e9d5a02d379511d08ee268351bf409a9ceb19
xk08 <gronytzki@gmail.com>
2021-11-29 15:16:10 -0300

    Aula dia 29-11-21 -> Versão prática feita em aula

--- Aula22_Animacoes/BarraDeProgresso/js/script.js
+++ Aula22_Animacoes/BarraDeProgresso/js/script.js
@@ -1,14 +1,68 @@
-function move() {
-    var elem = document.getElementById("myBar");
-    var width = 10;
-    var id = setInterval(frame, 10);
+
+var barraVerdeProgressoGlobal = 0;
+var barraLaranjaProgressoGlobal = 0;
+let barraVerdeTamanhoInicialGlobal = 0;
+let barraLaranjaTamanhoInicialGlobal = 0;
+let contadorGlobal = 1;
+
+function avancaBarraVerde(recebeValor) {
+
+    var elem = document.getElementById("barraVerdeProgresso");
+    var id = setInterval(frame, 10); // 1s == 1000 ms
+
+
     function frame() {
-        if (width >= 100) {
+        if (barraVerdeTamanhoInicialGlobal >= recebeValor) {
             clearInterval(id);
         } else {
-            width++;
-            elem.style.width = width + "%";
-            elem.innerHTML = width + "%";
+            barraVerdeTamanhoInicialGlobal++;
+            elem.style.width = barraVerdeTamanhoInicialGlobal + "%";
+            elem.innerHTML = barraVerdeTamanhoInicialGlobal + "%";
+        }
+    }
+}
+
+function avancaBarraLaranja(recebeValor) {
+
+    var elem = document.getElementById("barraLaranjaProgresso");
+    var id = setInterval(frame, 10); // 1s == 1000 ms
+
+
+    function frame() {
+        if (barraLaranjaTamanhoInicialGlobal >= recebeValor) {
+            clearInterval(id);
+        } else {
+            barraLaranjaTamanhoInicialGlobal++;
+            elem.style.width = barraLaranjaTamanhoInicialGlobal + "%";
+            elem.innerHTML = barraLaranjaTamanhoInicialGlobal + "%";
+        }
+    }
+}
+
+function avanca() {
+
+    if (barraLaranjaProgressoGlobal == 100) {
+        alert("Você já realizou as 5 interações previstas no sistema");
+    } else {
+
+        if (barraVerdeProgressoGlobal < 100) {
+            barraVerdeProgressoGlobal += 10; // avança de 10% em 10%
+            //Chama função que anima a barra e envia valor por parametro
+            avancaBarraVerde(barraVerdeProgressoGlobal);
+        } else {
+
+            if (barraLaranjaProgressoGlobal < 100) {
+                barraLaranjaProgressoGlobal += 20; // avança de 20% em 20%
+                avancaBarraLaranja(barraLaranjaProgressoGlobal);
+
+                //Reseta os valroes da barra-verde
+                barraVerdeProgressoGlobal = 0;
+                barraVerdeTamanhoInicialGlobal = 0;
+
+                //Exibe em tela o valor das interações realizadas
+                document.querySelector('h4').innerText = `Interações: 0${contadorGlobal++}`
+
+            }
         }
     }
 }
